af8d5f03ee0f725ba61caa2dbe4dd99febdfe50f
nmogylchuk <nataliia.mogylchuk@gmail.com>
2020-04-05 23:50:02 +0300

    Implement password change logic backend part

--- client/src/components/PasswordChange/PasswordChange.js
+++ client/src/components/PasswordChange/PasswordChange.js
@@ -1,42 +1,69 @@
-import React from "react";
+import React, { useContext, useState } from 'react';
+import { useHttp } from '../../hooks/http.hook';
+import { AuthContext } from '../../context/AuthContext';
+import { useHistory } from 'react-router-dom';
 
 
-const DriverPassword = () => {
+const PasswordChange = () => {
+    const history = useHistory();
+    const auth = useContext(AuthContext);
+    const { request } = useHttp();
+    const [form, setForm] = useState({
+        oldPassword: '',
+        newPassword1: '',
+        newPassword2: '',
+    });
+
+    const changeHandler = event => {
+        setForm({ ...form, [event.target.name]: event.target.value });
+    }
+
+    const updatePasswordHandler = async () => {
+        try {
+            const data = await request('/api/user', 'PATCH', { ...form },
+                { Authorization: `Bearer ${auth.token}` }
+            );
+            auth.logout();
+            history.push("/");
+        } catch (error) {
+            console.log('Catch error on updating password: ' + error)
+        }
+    };
+
+
     return (
         <div className='driver-password form'>
             <h2 className='form__item driver-password__title title'>Change Password</h2>
             <div className='form__wrapper'>
-
                 <div className='form__item'>
                     <div className='input'>
                         <div className='input__label'>
-                            <label htmlFor='password'>Old Password</label>
+                            <label htmlFor='oldPassword'>Old Password</label>
                         </div>
                         <div className='input__field-wrapper'>
                             <input
-                                name='password'
+                                name='oldPassword'
                                 className='input__field'
                                 type='password'
                                 placeholder='Password'
-                            //value={form.password}
-                            //onChange={changeHandler} 
-                            />
+                                value={form.oldPassword}
+                                onChange={changeHandler} />
                         </div>
                     </div>
                 </div>
                 <div className='form__item'>
                     <div className='input'>
                         <div className='input__label'>
-                            <label htmlFor='password'>New Password</label>
+                            <label htmlFor='newPassword1'>New Password</label>
                         </div>
                         <div className='input__field-wrapper'>
                             <input
-                                name='password'
+                                name='newPassword1'
                                 className='input__field'
                                 type='password'
                                 placeholder='New Password'
-                            //value={form.password}
-                            //onChange={changeHandler} 
+                                value={form.newPassword1}
+                                onChange={changeHandler}
                             />
                         </div>
                     </div>
@@ -44,26 +71,26 @@ const DriverPassword = () => {
                 <div className='form__item'>
                     <div className='input'>
                         <div className='input__label'>
-                            <label htmlFor='password'>Repeat New Password</label>
+                            <label htmlFor='newPassword2'>Repeat New Password</label>
                         </div>
                         <div className='input__field-wrapper'>
                             <input
-                                name='password'
+                                name='newPassword2'
                                 className='input__field'
                                 type='password'
                                 placeholder='Repeat New Password'
-                            //value={form.password}
-                            //onChange={changeHandler} 
+                                value={form.newPassword2}
+                                onChange={changeHandler}
                             />
                         </div>
                     </div>
                 </div>
                 <div className='submit__item'>
-                    <button className='driver-password__button button'>Change Password</button>
+                    <button className='driver-password__button button' onClick={updatePasswordHandler}>Change Password</button>
                 </div>
             </div>
         </div>
     )
 }
 
-export default DriverPassword;
+export default PasswordChange;
--- client/src/routes.js
+++ client/src/routes.js
@@ -4,10 +4,10 @@ import Welcome from './components/Welcome/Welcome';
 import Signup from './components/Signup/Signup';
 import Signin from './components/Signin/Signin';
 import Logout from './components/Logout/Logout';
+import PasswordChange from './components/PasswordChange/PasswordChange';
 
 import DriverWelcome from './components/Driver/Welcome';
 import DriverProfile from './components/Driver/Profile';
-import DriverPassword from './components/Driver/Password';
 import DriverTrucks from './components/Driver/Trucks/Trucks';
 import DriverCreateTruck from './components/Driver/Trucks/CreateTruck';
 import DriverUpdateTruck from './components/Driver/Trucks/UpdateTruck';
@@ -16,7 +16,6 @@ import DriverLoad from './components/Driver/Loads/Load';
 
 import ShipperWelcome from './components/Shipper/Welcome';
 import ShipperProfile from './components/Shipper/Profile';
-import ShipperPassword from './components/Shipper/Password';
 import ShipperProfileDelete from './components/Shipper/ProfileDelete';
 import ShipperLoads from './components/Shipper/Loads/Loads';
 import ShipperLoad from './components/Shipper/Loads/Load';
@@ -33,13 +32,13 @@ export const useRoutes = (isAuthenticated, userType) => {
                 <Switch>
                     <Route exact path="/driver" component={DriverWelcome} />
                     <Route exact path="/driver/profile" component={DriverProfile} />
-                    <Route exact path="/driver/profile/password" component={DriverPassword} />
+                    <Route exact path="/driver/profile/password" component={PasswordChange} />
                     <Route exact path="/driver/trucks" component={DriverTrucks} />
                     <Route exact path="/driver/truck/create" component={DriverCreateTruck} />
                     <Route exact path="/driver/truck/update/:id" component={DriverUpdateTruck} />
                     <Route exact path="/driver/loads" component={DriverLoads} />
                     <Route exact path="/driver/load/:id" component={DriverLoad} />
-                    <Route exact path="/logout" component={Logout}/>
+                    <Route exact path="/logout" component={Logout} />
                     <Redirect to="/driver" />
                 </Switch>
             );
@@ -50,13 +49,13 @@ export const useRoutes = (isAuthenticated, userType) => {
                 <Switch>
                     <Route exact path="/shipper" component={ShipperWelcome} />
                     <Route exact path="/shipper/profile" component={ShipperProfile} />
-                    <Route exact path="/shipper/profile/password" component={ShipperPassword} />
+                    <Route exact path="/shipper/profile/password" component={PasswordChange} />
                     <Route exact path="/shipper/profile/delete" component={ShipperProfileDelete} />
                     <Route exact path="/shipper/loads" component={ShipperLoads} />
                     <Route exact path="/shipper/load/create" component={ShipperCreateLoad} />
                     <Route exact path="/shipper/load/:id" component={ShipperLoad} />
                     <Route exact path="/shipper/load/update/:id" component={ShipperUpdateLoad} />
-                    <Route exact path="/logout" component={Logout}/>
+                    <Route exact path="/logout" component={Logout} />
                     <Redirect to="/shipper" />
                 </Switch>
             );
